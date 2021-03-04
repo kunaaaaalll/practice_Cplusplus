acf69aa30c01c4726175ffd412d1532f2fd39679
Kunal <10kunalji@gmail.com>
2021-03-04 18:55:05 +0530

    size of int

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+  
+using namespace std; 
+
+int main() {
+   cout<<"Hello World"<< endl;
+   cout<<"thats it!!"<<endl
+;
+    int a;
+    a=1; 
+    cout<< "size of int"<< sizeof(a)<<endl;
+    return 0;
+}
