8fe90865fc09792b4eff6bdd0e85b50804184425
Kunal <10kunalji@gmail.com>
2021-03-04 19:26:26 +0530

    odd even maxmin

---
+++ maxmin.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b){
+        if(a<c){
+            cout<<c<<endl;
+        }
+        else{
+            cout<<a<<endl;
+        }
+    }
+    else{
+        if(b<c){
+            cout<<c<<endl;
+        }
+        else{
+            cout<<b<<endl;
+        }
+    }
+    return 0;
+}
