fbb842e790c8cb1c6ddb3247ce35720aaee037b2
Kunal <10kunalji@gmail.com>
2021-03-04 19:01:36 +0530

    size of datatypes

--- 2.cpp
+++ 2.cpp
@@ -8,6 +8,17 @@ int main() {
 ;
     int a;
     a=1; 
-    cout<< "size of int"<< sizeof(a)<<endl;
+    cout<< "size of int "<< sizeof(a)<<endl;
+
+    float b;
+    b=3.14;
+    cout<< "size of float "<< sizeof(b)<<endl;
+    
+    bool c;
+    cout<<"size of boolean "<< sizeof(c)<<endl;
+    short int d;
+    cout<<"size of short int "<< sizeof(d)<<endl;
+    long int e;
+    cout<<"size of long int "<< sizeof(e)<<endl;
     return 0;
 }
