aa776fc64a393ac58975a34dd55a2d1b59cd0d56
kunaaaaalll <10kunalji@gmail.com>
2021-03-14 21:33:21 +0530

    cwh c++

---
+++ dowhileloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int sum=0;
+    int i=1;
+    do{
+        sum=sum+i;
+        i++;
+    }while(i<=n);
+    cout<<sum<<endl;
+    return 0; 
+}
